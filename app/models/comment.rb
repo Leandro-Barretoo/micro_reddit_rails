9f20e2fdd63c17a770866f4cac89d4ed25a716f6
AshakaE <ashakaegerega@gmail.com>
2021-02-10 07:48:08 +0100

    removed user and post validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  validates :user, presence: true
-  validates :post, presence: true
   validates :body, presence: true
 end
